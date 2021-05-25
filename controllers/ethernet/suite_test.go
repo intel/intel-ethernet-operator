065c1c3cab32a147fbadc89555facd63745d4827
kamilpoleszczuk <kamilx.poleszczuk@intel.com>
2021-05-25 09:13:40 +0200

    Fix license headers and linter error

--- controllers/ethernet/suite_test.go
+++ controllers/ethernet/suite_test.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2021 Intel Corporation
 
 package ethernet
 
@@ -40,6 +27,7 @@ import (
 var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
+var err error
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -58,7 +46,7 @@ var _ = BeforeSuite(func() {
 		ErrorIfCRDPathMissing: true,
 	}
 
-	cfg, err := testEnv.Start()
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
