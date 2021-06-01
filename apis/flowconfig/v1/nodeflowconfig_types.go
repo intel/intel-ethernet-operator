2ef02b49fc3d640684da89fda55a18aaff398900
Abdul Halim <abdul.halim@intel.com>
2021-06-01 13:04:43 +0100

    add NodeFlowConfig spec & status structs

--- apis/flowconfig/v1/nodeflowconfig_types.go
+++ apis/flowconfig/v1/nodeflowconfig_types.go
@@ -5,6 +5,7 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -14,12 +15,21 @@ import (
 type NodeFlowConfigSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Rules is a list of FlowCreate rules
+	Rules []*FlowRules `json:"rules,omitempty"`
 }
 
 // NodeFlowConfigStatus defines the observed state of NodeFlowConfig
 type NodeFlowConfigStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	PortInfo []PortsInformation `json:"portInfo"`
+	// Last applied rules
+	Rules      []*FlowRules   `json:"rules,omitempty"`
+	SyncStatus SyncStatusType `json:"syncStatus,omitempty"`
+	SyncMsg    string         `json:"syncMsg,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -45,4 +55,51 @@ type NodeFlowConfigList struct {
 
 func init() {
 	SchemeBuilder.Register(&NodeFlowConfig{}, &NodeFlowConfigList{})
+}
+
+type SyncStatusType string
+
+const (
+	SyncError    SyncStatusType = "Error"
+	SyncSuccess  SyncStatusType = "Success"
+	SyncProgress SyncStatusType = "Progress"
+)
+
+// PortsInformation defines port information
+type PortsInformation struct {
+	PortId   uint32 `json:"portId"`
+	PortPci  string `json:"portPci,omitempty"`
+	PortMode string `json:"portMode,omitempty"`
+}
+
+// FlowRules struct for flow rules creation and validation
+type FlowRules struct {
+	PortId  uint32        `json:"portId,omitempty"`
+	Attr    *FlowAttr     `json:"attr,omitempty"`
+	Pattern []*FlowItem   `json:"pattern,omitempty"`
+	Action  []*FlowAction `json:"action,omitempty"`
+}
+
+// FlowAttr defines Flow rule attributes
+type FlowAttr struct {
+	Group    uint32 `json:"group,omitempty"`
+	Priority uint32 `json:"priority,omitempty"`
+	Ingress  uint32 `json:"ingress,omitempty"`
+	Egress   uint32 `json:"egress,omitempty"`
+	Transfer uint32 `json:"transfer,omitempty"`
+	Reserved uint32 `json:"reserved,omitempty"`
+}
+
+// FlowItem defines flow pattern definition
+type FlowItem struct {
+	Type string                `json:"type,omitempty"`
+	Spec *runtime.RawExtension `json:"spec,omitempty"`
+	Last *runtime.RawExtension `json:"last,omitempty"`
+	Mask *runtime.RawExtension `json:"mask,omitempty"`
+}
+
+// FlowAction defines flow actions
+type FlowAction struct {
+	Type string                `json:"type,omitempty"`
+	Conf *runtime.RawExtension `json:"conf,omitempty"`
 }
