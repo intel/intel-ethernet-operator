5716dbcc2706ef4330dfcaf6cc8c234e714d51aa
Niamh Hennigan <niamh.hennigan@intel.com>
2022-11-07 13:40:26 +0000

    remove nil checks that are always true (#162)

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -17,15 +17,16 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strings"
 	"syscall"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/go-logr/logr"
 	configv1 "github.com/openshift/api/config/v1"
@@ -67,10 +68,7 @@ func IsOpenshiftSno(c client.Client, log logr.Logger) (bool, error) {
 	defaultInfraName := "cluster"
 	err := c.Get(context.TODO(), types.NamespacedName{Name: defaultInfraName}, infra)
 	if err != nil {
-		return false, err
-	}
-	if infra == nil {
-		return false, fmt.Errorf("getting resource Infrastructure (name: %s) succeeded but object was nil", defaultInfraName)
+		return false, fmt.Errorf("getting resource Infrastructure (name: %s) succeeded but object was empty", defaultInfraName)
 	}
 	log.Info("OCP cluster infrastructure", "infra", infra.Status.ControlPlaneTopology)
 	return infra.Status.ControlPlaneTopology == configv1.SingleReplicaTopologyMode, nil
